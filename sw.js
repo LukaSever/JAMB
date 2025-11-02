c2e4e8076677993cf17d0f6ebff2cdd82b396a8e
Luka Sever <lukasever143@email.com>
2025-11-03 00:31:55 +0100

    Projekat

--- sw.js
+++ sw.js
@@ -1,19 +1,19 @@
 const JAMB_CACHE = 'jamb-cache-v1';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/script.js',
-    '/manifest.json',
-    '/static/images/icon.png',
-    '/static/images/image0.png',
-    '/static/images/image1.png',
-    '/static/images/image2.png',
-    '/static/images/image3.png',
-    '/static/images/image4.png',
-    '/static/images/image5.png',
-    '/static/images/sum.png',
-    '/static/mp3/najava.mp3'
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './manifest.json',
+    './static/images/icon.png',
+    './static/images/image0.png',
+    './static/images/image1.png',
+    './static/images/image2.png',
+    './static/images/image3.png',
+    './static/images/image4.png',
+    './static/images/image5.png',
+    './static/images/sum.png',
+    './static/mp3/najava.mp3'
 ];
 
 self.addEventListener('install', (event) => {
